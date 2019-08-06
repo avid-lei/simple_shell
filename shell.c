f31a544656518f06829e5a6bf6b4d58d1aa6eefb
lei-diva <you@example.com>
2019-08-06 06:18:43 +0000

    take out construct

--- shell.c
+++ shell.c
@@ -5,18 +5,18 @@
 #include <sys/wait.h>
 #include "header.h"
 #include <signal.h>
-
-/**
+/*
+**
  * init - function line counter
  * Return: always 0
- */
+ *
 void init(void) __attribute__ ((constructor));
 void init(void)
 {
 	signal(SIGINT, sigHandler);
 
 }
-
+*/
 /**
  * main - main shell function
  * @argc: int
