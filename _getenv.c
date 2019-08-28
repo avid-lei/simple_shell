54dd4a5cf2899e4a17f7cef3174c0e511a7e1ab0
akeemseymens <812@holbertonschool.com>
2019-08-28 06:34:07 +0000

    betty compliant.

---
+++ _getenv.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <string.h>
+#include "header.h"
+/**
+ * _getenv - compares the enviormental varable to the path.
+ * @name: name of the pointer.
+ * Return: return the null.
+ *
+or **/
+char *_getenv(const char *name)
+{
+
+	extern char **environ;
+	int len = strlen(name);
+	int counter = 0;
+	char *path;
+
+	while (*environ)
+	{
+		if (strncmp((*environ), name, len) == 0)
+		{
+			path = _strdup(*environ + (len + 1));
+			environ = environ - counter;
+			return (path);
+
+		}
+
+		environ++;
+		counter++;
+	}
+
+
+	return (NULL);
+
+}
