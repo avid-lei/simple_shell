0aae56475417349386d0a7066345e4056c1d0bcc
akeemseymens <812@holbertonschool.com>
2019-08-28 06:13:54 +0000

    betty compliant there is a warning though

---
+++ int_getn.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+#include "header.h"
+/**
+* _getn - compares the string to environmental token returns the location.
+* @name: string name passing through
+* Return: return the location number.
+**/
+
+int _getn(const char *name)
+{
+
+	int x = 0;
+	extern char **environ;
+	int len = strlen(name);
+
+	for (x = 0; environ[x] != '\0'; x++)
+	{
+
+		if (strncmp(environ[x], name, len) == 0)
+		{
+
+			return (x);
+		}
+
+
+	}
+
+
+	return (-1);
+
+}
