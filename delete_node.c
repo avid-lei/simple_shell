8dba6d7161261066244affe39a0be02b7c1801e5
akeemseymens <812@holbertonschool.com>
2019-08-28 05:31:35 +0000

    created function for delete_node.

---
+++ delete_node.c
@@ -0,0 +1,37 @@
+/**
+ * delete_node - deletes the node of a list array.
+ * @head: double pointer.
+ * @index: unsigned int of the index.
+ *
+ * Return: is void.
+ **/
+
+void delete_node(list **head, unsigned int index)
+{
+	list *new = *head;
+	list *temp;
+	unsigned int counter = 0;
+
+
+	if (index == 0)
+	{
+		*head = (*head)->next;
+		free(new->s);
+		free(new);
+		return;
+	}
+	while (new)
+	{
+		if (index == counter + 1)
+		{
+			temp = new->next;
+			new->next = new->next->next;
+			free(temp);
+			return;
+		}
+		counter++;
+		new = new->next;
+	}
+
+	return;
+}
